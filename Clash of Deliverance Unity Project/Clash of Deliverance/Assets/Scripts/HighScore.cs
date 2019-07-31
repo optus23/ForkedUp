10c9de8035f206990b7b2588e926b425b8ea0d3d
Marc Galvez <36281630+optus23@users.noreply.github.com>
2019-08-01 00:15:44 +0200

    High score txt created

--- Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/HighScore.cs
+++ Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/HighScore.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
 
 public class HighScore : MonoBehaviour
 {
     Text high_score;
     public static int high_score_value;
+
+    string login;
+    string logout;
+   
 
     private void Awake()
     {
@@ -16,6 +22,7 @@ public class HighScore : MonoBehaviour
     void Start()
     {
         high_score = GetComponent<Text>();
+        CreateText();
     }
 
     // Update is called once per frame
@@ -27,5 +34,32 @@ public class HighScore : MonoBehaviour
             high_score_value = Score.score_value;
         }
 
+    }
+
+    private void OnApplicationQuit()
+    {
+        GameObject quadm = GameObject.Find("Quads");
+
+        string path = Application.dataPath + "/HighScore.txt";
+
+        logout = Environment.NewLine + "----Logout date:  " + System.DateTime.Now + "----" + Environment.NewLine;
+
+        File.AppendAllText(path, logout);
+    }
+
+    void CreateText()
+    {
+        string path = Application.dataPath + "/HighScore.txt";
+        //Create File 
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "PlayTesting - Variables Parsing" + Environment.NewLine + "Player:  <Set number>" + Environment.NewLine + "Name:	<Set name>" + Environment.NewLine + "Age:	<Set age>" + Environment.NewLine + "Gender:	<Set Gender>" + Environment.NewLine + "Experience:	< Low / Medium / High >" + Environment.NewLine);
+        }
+
+        //Content of the file
+        login = Environment.NewLine + "----Login date:  " + System.DateTime.Now + "----";
+
+        //Add some to text to it
+        File.AppendAllText(path, login);
     }
 }
