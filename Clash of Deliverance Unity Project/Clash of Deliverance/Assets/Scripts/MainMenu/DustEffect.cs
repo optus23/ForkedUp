b11d49b0e2b30f0622aed6e0b2e354e5c60121fa
Marc Galvez <36281630+optus23@users.noreply.github.com>
2019-08-01 00:16:14 +0200

    Dust Effect

---
+++ Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/MainMenu/DustEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustEffect : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
