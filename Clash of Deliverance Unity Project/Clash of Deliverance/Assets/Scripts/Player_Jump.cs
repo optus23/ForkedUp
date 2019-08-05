8f361a870765d72367bb14fc8235c47a5fcb3c33
Marc Galvez <36281630+optus23@users.noreply.github.com>
2019-08-05 18:59:50 +0200

    Jump animation

--- Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/Player_Jump.cs
+++ Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/Player_Jump.cs
@@ -9,52 +9,97 @@ public class Player_Jump : MonoBehaviour
 
     public float force;
     private Rigidbody2D rb2D;
+
     private bool key_down;
     private float initial_player_position_x;
-    private Quaternion initial_player_rotation;
+
     public static bool dead;
     public CameraShake cameraShake;
     private float time_death = 1.5f;
     private float timer;
     public GameObject Restart;
 
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+    public float smoothRotation;
+    private float timer_animation;
+    public float start_rotating;
+
+    Touch touch;
+
+
     // Start is called before the first frame update
     private void Awake()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         initial_player_position_x = gameObject.transform.position.x;
-        initial_player_rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
 
+        forwardRotation = Quaternion.Euler(0, 0, 30);
+        downRotation = Quaternion.Euler(0, 0, -70);
     }
 
     void Start()
     {
         //First Jump without Input Keydown
         rb2D.velocity = Vector2.zero;
-        rb2D.AddForce(transform.up * force);
+        rb2D.AddForce(Vector2.up * force);
         dead = false;
+        transform.rotation = Quaternion.Lerp(forwardRotation, transform.rotation, 2 * Time.deltaTime);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-       
-        if(Input.GetKeyDown("space") && !key_down && !dead)
+
+        if ((Input.GetKeyDown("space") || Input.touchCount > 0) && !key_down && !dead)
         {
+            if (Input.touchCount > 0)
+                touch = Input.GetTouch(0);
+
             rb2D.velocity = Vector2.zero;
-            rb2D.AddForce(transform.up * force);
+            rb2D.AddForce(Vector2.up * force);
             key_down = true;
+
+            //Animation
+            transform.rotation = forwardRotation;
+            Debug.Log(transform.rotation);
+
+            timer_animation = 0f;
+            smoothRotation = 1;
         }
-        else if (Input.GetKeyUp("space") && key_down)
+        else if ((Input.GetKeyUp("space") || Input.touchCount == 0) && key_down)
         {
             key_down = false;
+
         }
+        else
+        {
+            timer_animation += Time.deltaTime;
+            if (timer_animation > start_rotating)
+            {
+               
+                if (smoothRotation >= 4)
+                    smoothRotation += Mathf.Sqrt((smoothRotation / 10));
+                else
+                    smoothRotation += Mathf.Sqrt((smoothRotation / 200));
+                
+                if (smoothRotation >= 10)
+                    smoothRotation = 10;
+
+                transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, smoothRotation * Time.deltaTime);
+
+            }
+
+        }
+
+
+
 
         if (!dead)
         {
-            //Keep Vector initial position x & Quaterion rotation gameObject the same all time
+            //Keep Vector initial position.x
             gameObject.transform.position = new Vector2(initial_player_position_x, gameObject.transform.position.y);
-            gameObject.transform.rotation = initial_player_rotation;
+
         }
         else //Flip when die and SetActive Restart Button
         {
@@ -71,25 +116,21 @@ public class Player_Jump : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Obstacle")
-        {
-            Debug.Log("You are dead!");
+        //if (collision.transform.tag == "Obstacle")
+        //{
+        //    cameraShake.Shake(0.8f, 0.3f);
+        //    dead = true;
+        //    rb2D.AddForce(transform.right * -force);
+        //    rb2D.AddForce(transform.up * force);
 
-            cameraShake.Shake(0.8f, 0.3f);
-            dead = true;
-            rb2D.AddForce(transform.right * -force);
-            rb2D.AddForce(transform.up * force);
-
-        }
-        if (collision.transform.tag == "DeathFloor")
-        {
-            Debug.Log("You are dead!");
-
-            cameraShake.Shake(0.1f, 0.2f);
-            dead = true;
-            rb2D.AddForce(transform.right * (force / 2));
-            rb2D.AddForce(transform.up * (force / 2));
-        }
+        //}
+        //if (collision.transform.tag == "DeathFloor")
+        //{
+        //    cameraShake.Shake(0.1f, 0.2f);
+        //    dead = true;
+        //    rb2D.AddForce(transform.right * (force / 1.5f));
+        //    rb2D.AddForce(transform.up * (force / 1.5f));
+        //}
         if (collision.transform.tag == "Score")
         {
             Score.score_value += 1;
