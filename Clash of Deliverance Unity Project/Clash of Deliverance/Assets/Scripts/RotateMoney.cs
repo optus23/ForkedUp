7e96781a59a5cc3697f802cf0f1e37555440321c
Marc Galvez <36281630+optus23@users.noreply.github.com>
2019-08-07 23:09:18 +0200

    Money added

---
+++ Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/RotateMoney.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMoney : MonoBehaviour
+{
+     void Update()
+    {
+        gameObject.transform.Rotate(0, 6, 0);
+    }
+}
