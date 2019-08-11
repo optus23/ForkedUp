b3f43d5fc155d6d98ada3bdbf9bd39dbc268f0bd
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-08-11 14:08:08 +0200

    Credits and Shop and logi Main menu UI created

--- Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/LevelManager.cs
+++ Clash of Deliverance Unity Project/Clash of Deliverance/Assets/Scripts/LevelManager.cs
@@ -5,8 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    enum Panel_Stats
+    {
+        SHOP,
+        CREDITS,
+        RETURN_SHOP,
+        RETURN_CREDITS,
+        NONE
+    }
+
     Scene scene;
     Touch touch;
+
+    public GameObject panel;
+    public float velocity_panel;
+    public float offset_panel_shop;
+    public float offset_panel_play;
+    public float offset_panel_credits;
+
+    private bool  moving_panel;
+
+    private Panel_Stats panel_stats;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +41,13 @@ public class LevelManager : MonoBehaviour
         {
             if (Input.touchCount > 0)
                 touch = Input.GetTouch(0);
+
             TouchToJump();
+        }
+
+        if (scene.name == "Main Menu" && moving_panel)
+        {
+            MainMenuMovement();
         }
     }
 
@@ -50,6 +76,61 @@ public class LevelManager : MonoBehaviour
         SceneManager.LoadScene("WaitingPlayer");
         Player_Jump.dead = false;
     }
+    
+    public void ShopButton()
+    {
+        moving_panel = true;
+        panel_stats = Panel_Stats.SHOP;
+    }
+    public void ReturnShopButton()
+    {
+        moving_panel = true;
+        panel_stats = Panel_Stats.RETURN_SHOP;
+
+    }
+    public void CreditsButton()
+    {
+        moving_panel = true;
+        panel_stats = Panel_Stats.CREDITS;
+    }
+    public void ReturnCreditsButton()
+    {
+        moving_panel = true;
+        panel_stats = Panel_Stats.RETURN_CREDITS;
+
+    }
+
+
+
+    void MainMenuMovement()
+    {
+        //  Go Shop
+        if (Camera.main.transform.position.x <= offset_panel_shop && panel_stats == Panel_Stats.SHOP)
+        {
+
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + velocity_panel * Time.deltaTime, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        }
+        //  Go Credits
+        else if (Camera.main.transform.position.x >= offset_panel_credits && panel_stats == Panel_Stats.CREDITS)
+        {
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x - velocity_panel * Time.deltaTime, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+        }
+        //  Return  Shop
+        else if (Camera.main.transform.position.x >= offset_panel_play && panel_stats == Panel_Stats.RETURN_SHOP)
+        {
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x - velocity_panel * Time.deltaTime, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+        }
+        //  Return  Credits
+        else if (Camera.main.transform.position.x <= offset_panel_play && panel_stats == Panel_Stats.RETURN_CREDITS)
+        {
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + velocity_panel * Time.deltaTime, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+        }
+        else moving_panel = false;
+    }
+
 
     
 }
